dcfa2ad47e7feb4e48a8768bda2849ed599d2144
Benoît de CHATEAUVIEUX <bdechateauvieux@exoplatform.com>
2015-05-22 12:00:40 +0700

    add Copyright headers

--- src/main/java/org/exoplatform/swf/sonar/plugin/rules/WebUiCsrfCheck.java
+++ src/main/java/org/exoplatform/swf/sonar/plugin/rules/WebUiCsrfCheck.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2003-2010 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
 package org.exoplatform.swf.sonar.plugin.rules;
 
 import org.sonar.api.server.rule.RulesDefinition;
@@ -10,9 +26,6 @@ import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 
-/**
- * Created by bdechateauvieux on 5/12/15.
- */
 @Rule(key = "WebUiCsrfCheck",
         name = "WebUI CSRF protection check",
         description = "This rule checks that all the WebUI action listeners have CSRF protection activated",
